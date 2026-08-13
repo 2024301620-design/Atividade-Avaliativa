30663daa04a33c9d92f926b2cd71aef0eb3fd5b1
Leo Asch <2024301620@aluno.canoas.ifrs.edu.br>
2026-08-13 09:36:06 -0300

    Add files via upload

---
+++ Estudante.java
@@ -0,0 +1,20 @@
+public class Estudante {
+    // Atributos
+    private String nome;
+    private double[] notas;
+
+    //Construtor
+    public Estudante (String nome){
+        this.nome = nome;
+        this.notas = new double[5];
+    }
+
+    //Metodos
+    public void insereNotas(){
+        Scanner scanner = new Scanner(System.in);
+        for(int i = 0; i < 5; i++){
+        System.out.println("Insira a " + i+1 + "° nota: ");
+        double notas[i] = scanner.nextDouble;
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void Main(String[] args) {
+    Estudante estudante = new Estudante('leonardo');
+    estudante.inserirNotas();
+    }
+}
